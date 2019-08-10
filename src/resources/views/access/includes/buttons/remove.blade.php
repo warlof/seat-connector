6ddfcffceace45cf688ae16bdab7dd2623261284
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-08-10 10:50:32 +0200

    fix(access): ensure remove button is sending set_id

--- src/resources/views/access/includes/buttons/remove.blade.php
+++ src/resources/views/access/includes/buttons/remove.blade.php
@@ -1,7 +1,7 @@
 <form method="post" action="{{ route('seat-connector.acl.remove') }}">
   {{ csrf_field() }}
   {{ method_field('DELETE') }}
-  <input type="hidden" name="permission_group_id" value="{{ $row->id }}" />
+  <input type="hidden" name="set_id" value="{{ $row->id }}" />
   <input type="hidden" name="entity_type" value="{{ $row->entity_type }}" />
   <input type="hidden" name="entity_id" value="{{ $row->entity_id }}" />
   <button type="submit" class="btn btn-xs btn-danger pull-right">{{ trans('seat-connector::seat.remove') }}</button>
