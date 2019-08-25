36ea99f8fa20ad485692fb2e51e29ca859500246
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-08-25 15:35:25 +0200

    fix(settings): provide a way to define security level, ticker and trigger commands

--- src/resources/views/settings/list.blade.php
+++ src/resources/views/settings/list.blade.php
@@ -5,39 +5,166 @@
 
 @section('full')
 
-  @foreach($drivers->split(abs($drivers->count() / 3)) as $row)
+  <div class="row">
 
-    @foreach($row as $key => $driver)
+    <div class="col-md-3">
 
-      <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Common</h3>
+        </div>
+        <div class="panel-body">
+          <form method="post" id="seat-connector-setup">
+            <div class="form-group">
+              <label for="security-level">Security Level</label>
+              <select id="security-level" name="security-level" class="form-control">
+                <option>Strict</option>
+                <option>Normal</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="use-ticker">Use ticker ?</label>
+              <div class="radio">
+                <label>
+                  @if(setting('seat-connector.ticker', true))
+                    <input type="radio" id="use-ticker" name="use-ticker" value="false" /> No
+                  @else
+                    <input type="radio" id="use-ticker" name="use-ticker" value="false" checked /> No
+                  @endif
+                </label>
+                <label>
+                  @if(setting('seat-connector.ticker', true))
+                  <input type="radio" id="use-ticker" name="use-ticker" checked /> Yes
+                  @else
+                  <input type="radio" id="use-ticker" name="use-ticker" /> Yes
+                  @endif
+                </label>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="prefix-format">Prefix mask</label>
+              @if(setting('seat-connector.format', true) == '')
+                <input type="text" id="prefix-format" name="prefix-format" class="form-control" value="[%s] %s" />
+              @else
+                <input type="text" id="prefix-format" name="prefix-format" class="form-control" value="{{ setting('seat-connector.format', true) }}" />
+              @endif
+            </div>
+          </form>
+        </div>
+        <div class="panel-footer clearfix">
+          <button type="button" class="btn btn-success pull-right" form="seat-connector-setup">{{ trans('seat-connector::seat.save') }}</button>
+        </div>
+      </div>
 
-        <div class="box box-solid">
-          <div class="box-header with-border">
-            <i class="fa {{ $driver->icon }}"></i>
-            <h4 class="box-title">{{ ucfirst($driver->name) }}</h4>
+    </div>
+
+    <div class="col-md-9">
+
+      <div class="panel panel-default">
+
+        <div class="panel-heading">
+          <h3 class="panel-title">Management</h3>
+        </div>
+
+        <div class="panel-body">
+
+          <div class="col-md-6 no-padding">
+
+            <div class="row">
+
+              <div class="col-md-6">
+                <button type="button" role="button" class="btn btn-success btn-block btn-flat">Update Sets</button>
+                <span class="help-block">This will update known Sets from selected Drivers.</span>
+              </div>
+
+              <div class="col-md-6">
+                <button type="button" role="button" class="btn btn-danger btn-block btn-flat">Reset Everybody</button>
+                <span class="help-block">This will remove all Set from every Users on selected Drivers.</span>
+              </div>
+
+            </div>
+
+            <div class="row">
+
+              <div class="col-md-6">
+                <button type="button" role="button" class="btn btn-success btn-block btn-flat">Sync. Users</button>
+                <span class="help-block">This will apply Users policy to selected Drivers.</span>
+              </div>
+
+              <div class="col-md-6">
+                <select class="form-control">
+                  <option value="">All Drivers</option>
+                  @foreach($drivers as $key => $driver)
+                  <option value="{{ $key }}">{{ ucfirst($driver->name) }}</option>
+                  @endforeach
+                </select>
+                <span class="help-block">This will determine for which driver action have to be queue.</span>
+              </div>
+
+            </div>
+
           </div>
-          <div class="box-body">
-            <form role="form" method="post" id="{{ sprintf('seat-connector-%s', $key) }}" action="{{ route(sprintf('seat-connector.drivers.%s.settings', $key)) }}">
-              {{ csrf_field() }}
 
-              @foreach($driver->settings as $field)
-                <div class="form-group">
-                  <label for="{{ sprintf('seat-connector-%s-%s', $key, $field->name) }}">{{ trans($field->label) }}</label>
-                  <input type="text" name="{{ $field->name }}"
-                         id="{{ sprintf('seat-connector-%s-%s', $key, $field->name) }}" class="form-control"
-                         value="{{ $field->value }}" />
-                </div>
-              @endforeach
-            </form>
+          <div class="col-md-6">
+
+            <dl class="dl-horizontal">
+              <dt>Security Level</dt>
+              <dd>Determine how policy must be applied. Using <code>strict</code> will require user have all their linked character with a valid ESI access. Otherwise, they will loose all their access.</dd>
+
+              <dt>Use Ticker</dt>
+              <dd>If yes is selected, the Character Corporation Ticker will be added to the name according to <code>prefix mask</code>.</dd>
+
+              <dt>Prefix mask</dt>
+              <dd>This is the pattern which will define how ticker must be put. <code>%s</code> is a Joker. They will be replaced in the following order : <i>ticker</i>, <i>character name</i>.</dd>
+            </dl>
+
           </div>
-          <div class="box-footer">
-            <button type="submit" form="{{ sprintf('seat-connector-%s', $key) }}" class="btn btn-success pull-right">{{ trans('seat-connector::seat.save') }}</button>
-          </div>
+
         </div>
 
       </div>
 
-    @endforeach
+    </div>
+
+  </div>
+
+  @foreach($drivers->split(abs($drivers->count() / 3)) as $row)
+
+    <div class="row">
+
+      @foreach($row as $key => $driver)
+
+        <div class="col-md-3">
+
+          <div class="box box-solid">
+            <div class="box-header with-border">
+              <i class="fa {{ $driver->icon }}"></i>
+              <h4 class="box-title">{{ ucfirst($driver->name) }}</h4>
+            </div>
+            <div class="box-body">
+              <form role="form" method="post" id="{{ sprintf('seat-connector-%s', $key) }}" action="{{ route(sprintf('seat-connector.drivers.%s.settings', $key)) }}">
+                {{ csrf_field() }}
+
+                @foreach($driver->settings as $field)
+                  <div class="form-group">
+                    <label for="{{ sprintf('seat-connector-%s-%s', $key, $field->name) }}">{{ trans($field->label) }}</label>
+                    <input type="text" name="{{ $field->name }}"
+                           id="{{ sprintf('seat-connector-%s-%s', $key, $field->name) }}" class="form-control"
+                           value="{{ $field->value }}" />
+                  </div>
+                @endforeach
+              </form>
+            </div>
+            <div class="box-footer">
+              <button type="submit" form="{{ sprintf('seat-connector-%s', $key) }}" class="btn btn-success pull-right">{{ trans('seat-connector::seat.save') }}</button>
+            </div>
+          </div>
+
+        </div>
+
+      @endforeach
+
+    </div>
 
   @endforeach
 
