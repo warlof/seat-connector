5a7adb669322dd14175359ca42db821d10015738
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-11-08 22:40:15 +0100

    fix(cve): enforce registration security

---
+++ src/database/migrations/2019_11_08_221506_add_unique_on_connector_unique_id_to_seat_connector_users_table.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of seat-connector and provides user synchronization between both SeAT and third party platform
+ *
+ * Copyright (C) 2019  Loïc Leuilliot <loic.leuilliot@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class AddUniqueOnConnectorUniqueIDToSeatConnectorUsersTable.
+ */
+class AddUniqueOnConnectorUniqueIDToSeatConnectorUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('seat_connector_users', function (Blueprint $table) {
+            $table->unique(['connector_type', 'unique_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('seat_connector_users', function (Blueprint $table) {
+            $table->dropUnique(['connector_type', 'unique_id']);
+        });
+    }
+}
