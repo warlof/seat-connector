2548c8c912e39ca5e1dcd0092856347f6dd22f69
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-03-26 20:55:34 +0100

    fix(ui): replace description structure with classic grid
    With adminLTE3, the description structure does non longer have an horizontal render.
    Therefore, we replace it by a standard grid system based on row, column and bold titles.

--- src/resources/views/identities/includes/card.blade.php
+++ src/resources/views/identities/includes/card.blade.php
@@ -4,38 +4,56 @@
       <h4 class="text-uppercase">
         <strong>{{ $metadata['name'] }}</strong>
       </h4>
-      <dl class="dl-horizontal">
-        <dt>Name</dt>
-        <dd>
+      <div class="row">
+        <div class="col-4 text-right">
+          <b>Name</b>
+        </div>
+        <div class="col-8">
           @if($identity = $identities->where('connector_type', $driver)->first())
             <i>{{ $identity->connector_name }}</i>
           @endif
-        </dd>
-        <dt>Unique ID</dt>
-        <dd>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-right">
+          <b>Unique ID</b>
+        </div>
+        <div class="col-8">
           @if($identity = $identities->where('connector_type', $driver)->first())
             <i>{{ $identity->unique_id }}</i>
           @endif
-        </dd>
-        <dt>Created on</dt>
-        <dd>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-right">
+          <b>Created on</b>
+        </div>
+        <div class="col-8">
           @if($identity = $identities->where('connector_type', $driver)->first())
             <i>{{ $identity->created_at }}</i>
           @endif
-        </dd>
-        <dt>Status</dt>
-        <dd>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-right">
+          <b>Status</b>
+        </div>
+        <div class="col-8">
           @if($identities->where('connector_type', $driver)->isNotEmpty())
-            <i class="text-green">registered</i>
+            <span class="badge badge-success p-1">
+              <i class="fas fa-check-circle mr-1"></i> Registered
+            </span>
           @else
-            <i class="text-red">unregistered</i>
+            <span class="badge badge-danger p-1">
+              <i class="fas fa-times-circle mr-1"></i> Unregistered
+            </span>
           @endif
-        </dd>
-      </dl>
+        </div>
+      </div>
     </div>
     @if(array_key_exists('icon', $metadata))
       <div class="icon">
-        <i class="fa {{ $metadata['icon'] }}"></i>
+        <i class="{{ $metadata['icon'] }}"></i>
       </div>
     @endif
     <a class="small-box-footer" href="{{ route(sprintf('seat-connector.drivers.%s.registration', $driver)) }}" target="_blank">
