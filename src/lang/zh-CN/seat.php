61ea7d70c0de4993ffa8c8bf9e906841554d5987
Echo <git@omniarch.io>
2020-09-22 17:12:38 -0700

    add Chinese localization
    (cherry picked from commit 72bffedd293d509f8a3806579dc384370c397bf6)

---
+++ src/lang/zh-CN/seat.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'settings'           => '设置',
+    'save'               => '保存',
+
+    'toolbox'            => '工具箱',
+    'access_management'  => '访问管理',
+    'user_mapping'       => '用户映射',
+    'journal'            => '日志',
+
+    'all_filter'         => '所有过滤器',
+    'public_filter'      => '公共过滤器',
+    'user_filter'        => '用户过滤器',
+    'role_filter'        => '角色过滤器',
+    'corporation_filter' => '公司过滤器',
+    'title_filter'       => '头衔过滤器',
+    'alliance_filter'    => '联盟过滤器',
+
+    'driver'             => '驱动程序',
+    'sets'               => '组|组',
+    'access'             => '访问',
+    'entity_name'        => '实体名',
+
+    'remove'             => '移除',
+    'help'               => '帮助',
+
+    'datetime'           => '日期/时间',
+    'category'           => '分类',
+    'message'            => '消息',
+    'level'              => '级别',
+
+    'group_id'           => '组ID',
+    'character_id'       => '人物 ID',
+    'character_name'     => '人物名',
+    'connector_id'       => '授权器 ID',
+    'connector_name'     => '授权器名',
+
+    'identities'         => '身份认证|身份认证',
+];
